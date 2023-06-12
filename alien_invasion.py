d03a3e6e886265419608159fabfc5f12e3a6f727
abubaker khaled <abubakr_20210011@fci.helwan.edu.eg>
2023-06-12 11:22:38 +0300

    add try-catch block to the code.

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,24 +5,26 @@ from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group  # changed _Group to Group
 
+try:
+    def run_game():
+        # Initialize the game and create the a screen object.
+        pygame.init()
+        ai_settings = Settings()
+        screen = pygame.display.set_mode(
+            (ai_settings.screen_width, ai_settings.screen_height))
+        pygame.display.set_caption('Alien Invasion')
+        # Make a ship
+        ship = Ship(ai_settings, screen)
+        # Make a group to store the bullets in.
+        bullets = Group()  # changed _Group to Group
+        # Start the main loop of the game.
+        while True:
+            gf.check_events(ai_settings, screen, ship, bullets)
+            ship.update()
+            bullets.update()
+            gf.update_screen(ai_settings, screen, ship, bullets)
 
-def run_game():
-    # Initialize the game and create the a screen object.
-    pygame.init()
-    ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption('Alien Invasion')
-    # Make a ship
-    ship = Ship(ai_settings, screen)
-    # Make a group to store the bullets in.
-    bullets = Group()  # changed _Group to Group
-    # Start the main loop of the game.
-    while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-        gf.update_screen(ai_settings, screen, ship, bullets)
-
-
-run_game()
+    run_game()
+except Exception as e:
+    print(e)
+    # handle the exception
