8ac47a7ba2a02591c96eda6950cec9a798366ed2
abubaker khaled <abubakr_20210011@fci.helwan.edu.eg>
2023-06-15 11:38:41 +0300

    Add Comments to code.

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,13 +16,19 @@ try:
         screen = pygame.display.set_mode(
             (ai_settings.screen_width, ai_settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
+
         # Make a ship
         ship = Ship(ai_settings, screen)
+
         # Make a group to store the bullets in.
         bullets = Group()  # changed _Group to Group
+
         # Start the main loop of the game.
         while True:
+
+            # Call the check events function to handle events.
             gf.check_events(ai_settings, screen, ship, bullets)
+
             ship.update()
             bullets.update()
             gf.update_screen(ai_settings, screen, ship, bullets)
