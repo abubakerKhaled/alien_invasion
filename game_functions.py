0a6d54f3b2e67695c53e50db50e064fcb21aa89b
abubaker khaled <abubakr_20210011@fci.helwan.edu.eg>
2023-06-15 12:13:03 +0300

    Adding comments.

--- game_functions.py
+++ game_functions.py
@@ -21,28 +21,35 @@ def check_events(ai_settings, screen, ship, bullets):
 
 def check_keydown_event(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses."""
+
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
+
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+
     elif event.key == pygame.K_SPACE:
         # Create new bullet and add it to the bullets group.
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
+
     elif event.key == pygame.K_w and event.mod & pygame.KMOD_CTRL:
         sys.exit()
 
 
 def check_keyup_event(event, ship):
     """Respond to key releases."""
+
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
+
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship, bullets):
     """Redraw the screen during each pass through the loop."""
+
     # Redraw all bullets behind ship and aliens.
     for bullet in bullets.sprites():
         bullet.draw_bullet()
