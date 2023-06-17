e44a093a8f05844b954ea25ec72c05ea568c4905
abubaker khaled <abubakr_20210011@fci.helwan.edu.eg>
2023-06-17 17:35:32 +0300

    Add alien.

---
+++ alien.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """A clas to represent a single alien in the feet."""
+
+    def __init__(self, ai_settings, screen):
+        """Initialize the alien and set its starting position."""
+        super(Alien, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Loat the alien image and set its rect attribute
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.screen.width
+        self.rect.y = self.screen.height
+
+        # Store the alien's exact position.
+        self.x = float(self.rect.x)
+
+    def blime(self):
+        """Draw the alien at its current position."""
+        self.screen.blime(self.image, self.rect)
