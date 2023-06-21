25d40cf8849b8c4cf00e80c0c55c93ce2ac1991f
abubaker khaled <abubakr_20210011@fci.helwan.edu.eg>
2023-06-21 19:45:55 +0300

    change the alien settings.

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ class Settings():
         self.bullets_allowed = 3
 
         # Alien Settings
-        self.alien_speed_factor = 1
-        self.fleet_drop_speed = 5
+        self.alien_speed_factor = 0.5
+        self.fleet_drop_speed = 1
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
